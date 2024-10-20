2e4c882202421967800cb4fdbdc770725b95e956
Truong Dang Duong <truongdangduong666@gmail.com>
2024-10-20 18:06:06 +0700

    upload code

---
+++ data/Hang.js
@@ -0,0 +1,56 @@
+
+const configs = {
+    "owner" : "Thanh Hằng"
+}
+
+const letters = [
+    {
+        "from": "Đăng Dương",
+        "content": "Hi lô cậu",
+        "hasHeader": true,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Hôm nay là 20/10 rùi đó",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Và cả 300 + 1 ngày mình yêu nhau nữaa",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Thế là sắp tròn một năm tớ tỏ tình cậu gòi",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Cảm ơn cậu đã luôn yêu thương tớ và dành cho tớ những thứ đáng yêu nhấtt",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Cậu chia sẻ niềm vui với tớ này, động viên tớ khi tớ buồn",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Hihi thíc lắm ạ",
+        "hasHeader": false,
+        "hasFooter": false,
+    },
+    {
+        "from": "Đăng Dương",
+        "content": "Ngày 20-10 tớ chúc cậu luôn mạnh khoẻ này, may mắn này, xinh đẹp này, học giỏi này, và yêu tớ nhiều hơn nữaa <br><span class=\"text-[#F3778E]\">Tớ yêu cậu nhiềuuuu!</span> ",
+        "hasHeader": false,
+        "hasFooter": true,
+    },
+    
+]
